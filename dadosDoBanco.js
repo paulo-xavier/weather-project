998418f8107275aacd3d8e0af80de31fca4e9e6d
AlexandreBitelo <alexandrebitelo41@gmail.com>
2023-11-23 17:38:10 -0300

    Arquivos do projeto

---
+++ dadosDoBanco.js
@@ -0,0 +1,61 @@
+
+adicionarAoBanco("São Leopoldo", "21", "chuvoso", "15/03/23 15:57", "12 km/h", "32", "Brasil");
+
+adicionarAoBanco ("São Paulo", "23", "ensolarado", "23/05/23 20:45", "40 km/h", "32", "Brasil");
+
+adicionarAoBanco ("Jacarezinho", "34", "neblina", "30/05/23 19:45", "37 km/h", "20", "Brasil");
+
+adicionarAoBanco ("Rio de Janeiro", "25", "parcialmente nublado", "23/05/23 19:30", "35 km/h", "28", "Brasil");
+
+adicionarAoBanco ("Belo Horizonte", "20", "chuvoso", "23/05/23 18:15", "25 km/h", "22", "Brasil");
+
+adicionarAoBanco ("Recife", "28", "ensolarado", "23/05/23 21:00", "30 km/h", "31", "Brasil");
+
+adicionarAoBanco ("Porto Alegre", "18", "nebuloso", "23/05/23 17:00", "20 km/h", "20", "Brasil");
+
+adicionarAoBanco ("Salvador", "26", "chuva forte", "23/05/23 22:30", "28 km/h", "30", "Brasil");
+
+adicionarAoBanco ("Curitiba", "15", "céu limpo", "23/05/23 16:15", "18 km/h", "18", "Brasil");
+
+adicionarAoBanco ("Fortaleza", "30", "tempestade", "23/05/23 23:45", "32 km/h", "33", "Brasil");
+
+adicionarAoBanco ("11", "Brasília", "22", "parcialmente nublado", "23/05/23 18:45", "25 km/h", "26", "Brasil");
+
+adicionarAoBanco ("Manaus", "32", "ensolarado", "23/05/23 15:30", "28 km/h", "30", "Brasil");
+
+adicionarAoBanco ("Florianópolis", "17", "neblina", "23/05/23 07:00", "15 km/h", "19", "Brasil");
+
+adicionarAoBanco ("Goiânia", "28", "chuva leve", "23/05/23 20:00", "22 km/h", "27", "Brasil");
+
+adicionarAoBanco ("Natal", "29", "nuvens esparsas", "23/05/23 16:45", "26 km/h", "28", "Brasil");
+
+adicionarAoBanco ("Maceió", "27", "trovoada", "23/05/23 21:15", "30 km/h", "29", "Brasil");
+
+adicionarAoBanco ("Porto Velho", "26", "parcialmente nublado", "23/05/23 19:30", "24 km/h", "28", "Brasil");
+
+adicionarAoBanco ("Cuiabá", "31", "céu limpo", "23/05/23 17:15", "26 km/h", "32", "Brasil");
+
+adicionarAoBanco ("João Pessoa", "25", "chuva moderada", "23/05/23 22:45", "20 km/h", "26", "Brasil");
+
+adicionarAoBanco ("Teresina", "30", "ensolarado", "23/05/23 14:30", "28 km/h", "31", "Brasil");
+
+adicionarAoBanco ("Campo Grande", "24", "nebuloso", "23/05/23 18:00", "23 km/h", "25", "Brasil");
+
+adicionarAoBanco ("Vitória", "23", "chuva fraca", "23/05/23 23:30", "18 km/h", "24", "Brasil");
+
+adicionarAoBanco ("Aracaju", "26", "tempestade", "23/05/23 16:00", "32 km/h", "30", "Brasil");
+
+adicionarAoBanco ("Palmas", "29", "céu limpo", "23/05/23 17:45", "25 km/h", "27", "Brasil");
+
+adicionarAoBanco ("Boa Vista", "33", "ensolarado", "23/05/23 20:30", "30 km/h", "34", "Brasil");
+
+adicionarAoBanco ("Macapá", "28", "parcialmente nublado", "23/05/23 19:15", "22 km/h", "29", "Brasil");
+
+adicionarAoBanco ("Belém", "31", "chuva forte", "23/05/23 22:45", "28 km/h", "33", "Brasil");
+
+adicionarAoBanco ("São Luís", "27", "céu limpo", "23/05/23 15:30", "25 km/h", "28", "Brasil");
+
+adicionarAoBanco ("Rio Branco", "30", "trovoada", "23/05/23 21:00", "30 km/h", "31", "Brasil");
+
+adicionarAoBanco ("Curitiba", "18", "chuva fraca", "23/05/23 14:45", "20 km/h", "22", "Brasil");
+
