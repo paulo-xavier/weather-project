7b6ca258f00181f5f59dce5a9135b10cc39c8aa0
paulo-xavier <px568661@gmail.com>
2023-11-24 05:18:40 -0300

    update

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,49 +1,97 @@
-// const fetch = require('node-fetch');
+
+
+const buttonSubmit = document.getElementById('button-submit');
+
+// const imageTemperature = document.getElementsByClassName("image-temperature");
 
 
 
-// const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=boston';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': '642ed199ebmsh227970cf574527bp1bf74bjsn44f6e337a112',
-//     'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-//   }
-// };
+buttonSubmit.addEventListener('click', () => {
+    console.log("clickou");
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-
-
-// const valueUser = document.getElementById('city').value; 
-// let valueUser = document.getElementsByTagName("input")[0].value;
-// console.log(valueUser); 
+    const cityInput = document.getElementById('city').value;
+    console.log(cityInput);
 
 
 
-let buttonSubmit = document.getElementById('button-submit'); 
+    getCityInfo(cityInput);
 
-
-buttonSubmit.addEventListener('click', ()  => {
-      console.log("clickou"); 
-
-      let valueUser = document.getElementById('city').value; 
-      console.log(valueUser); 
-      
-
-      
-
-
-}); 
+});
 
 
 
+function getCityInfo(cityInput) {
+    apiUrl = `https://projeto-tempo.onrender.com/cidade/${cityInput}/json`;
+
+    fetch(apiUrl)
+
+        .then(res => res.json())
+        .then((data) => {
+
+            printInScreen(data)
+
+
+        })
+
+}
+
+function printInScreen(data) {
+
+    const temperature = data.temperatura;
+    const city = data.cidade;
+    const localTime = data.data_local;
+    const weather = data.estado;
+    const windSpeed = data.velocidade_do_vento;
+
+
+    document.getElementById("temperature").textContent = `${temperature}°C`;
+    document.getElementById("city-location").textContent = city;
+    document.getElementById("localtime").textContent = localTime;
+    document.getElementById("weather").textContent = `Clima: ${weather}`;
+    document.getElementById("wind-speed").textContent = `Velocidade do vento: ${windSpeed}`;
+
+    changeImageAndBackground(weather);
+
+
+}
+
+
+function changeImageAndBackground(weather) {
+    let image = document.getElementById("image-temperature");
+    let containerPage = document.getElementById("container-page");
+
+    if (weather == "chuvoso" || weather == "chuva fraca" || weather == "chuva forte" || weather == "chuva leve" || weather == "chuva moderada") {
+        image.setAttribute("src", "./assets/images/rainy.png");
+
+        // containerPage.style.background = 'linear-gradient(180deg, #020C14 0%, rgba(59, 142, 213, 0.89) 99.99%, rgba(70, 170, 255, 0.00) 100%);'
+
+        containerPage.style.background = 'linear-gradient(180deg, #020C14 0%, rgba(59, 142, 213, 0.89) 99.99%, rgba(70, 170, 255, 0.00) 100%)';
 
 
 
+    } else if (weather == "ensolarado" || weather == "céu limpo") {
+        image.setAttribute("src", "./assets/images/sunny.png");
+        let containerPage = document.getElementById("container-page");
 
+        // document.getElementById("container-page").style.background = 'linear-gradient(180deg, #020C14 0%, rgba(59, 142, 213, 0.89) 99.99%, rgba(70, 170, 255, 0.00) 100%)';
+
+        containerPage.style.background = 'linear-gradient(180deg, #46AAFF 0%, rgba(59, 142, 213, 0.89) 99.99%, rgba(70, 170, 255, 0.00) 100%);'
+
+
+    } else if (weather == "parcialmente nublado" || weather == "nebuloso" || weather == "nuvens esparsas") {
+        image.setAttribute("src", "./assets/images/cloudy.png");
+
+        containerPage.style.background = 'linear-gradient(180deg, #4778A1 0%, rgba(59, 142, 213, 0.89) 99.99%, rgba(70, 170, 255, 0.00) 100%)';
+
+
+    } else {
+        image.setAttribute("src", "./assets/images/stormy.png");
+
+        containerPage.style.background = 'linear-gradient(180deg, #175081 0%, rgba(0, 96, 177, 0.89) 99.99%, rgba(70, 170, 255, 0.00) 100%)'
+
+
+    }
+
+
+
+}
